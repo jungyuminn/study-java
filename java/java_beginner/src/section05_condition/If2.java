46271a98a3ec4197fe035006f77204ac1be89d7a
jungyuminn <jungym0419@gmail.com>
2025-10-18 20:13:21 +0900

    feat: if-else 조건문

---
+++ java/java_beginner/src/section05_condition/If2.java
@@ -0,0 +1,13 @@
+package section05_condition;
+
+public class If2 {
+    public static void main(String[] args) {
+        int age = 20;
+
+        if (age >= 20) {
+            System.out.print("성입입니다.");
+        } else {
+            System.out.println("미성년자입니다.");
+        }
+    }
+}
